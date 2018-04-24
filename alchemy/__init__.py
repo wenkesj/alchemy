acd6e2b1a845ba700ab07ad9f3d8f08557638646
Sam Wenke <samwenke@gmail.com>
2018-04-24 14:33:47 -0400

    fix dims in fast weights

--- alchemy/__init__.py
+++ alchemy/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-from alchemy import contrib
-from alchemy import multiprocessing
-from alchemy import utils
+from . import contrib
+from . import multiprocessing
+from . import utils
