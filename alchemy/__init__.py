6a19e6ddbbf68a1d1408703ae8873bb0fd2cbf22
Sam Wenke <samwenke@gmail.com>
2018-04-24 12:18:41 -0400

    refactor

--- alchemy/__init__.py
+++ alchemy/__init__.py
@@ -1,2 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+
+from alchemy import contrib
+from alchemy import multiprocessing
+from alchemy import utils
--- alchemy/contrib/__init__.py
+++ alchemy/contrib/__init__.py
@@ -1,2 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+
+from alchemy.contrib import data
+from alchemy.contrib import envs
+from alchemy.contrib import layers
+from alchemy.contrib import rnn
