7ca538291bd9f55eb9680386124a1c1ff7b2b019
Sam Wenke <samwenke@gmail.com>
2018-05-07 12:49:08 -0400

    fix imports

--- alchemy/__init__.py
+++ alchemy/__init__.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 from . import contrib
 from . import multiprocessing
 from . import utils
-from . import layers
-from . import rl
-from . import rnn
-from . import train
+from .contrib import layers as layers
+from .contrib import rl as rl
+from .contrib import rnn as rnn
+from .contrib import train as train
