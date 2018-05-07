dc5e3a67f0891018aeb5870bb5e36a59fccee5be
Sam Wenke <samwenke@gmail.com>
2018-05-07 13:45:58 -0400

    found a better way to do it

--- alchemy/contrib/rl/__init__.py
+++ alchemy/contrib/rl/__init__.py
@@ -5,5 +5,4 @@ from __future__ import absolute_import
 from alchemy.contrib.rl.dataset import *
 from alchemy.contrib.rl.experience import *
 from alchemy.contrib.rl.ops import *
-from alchemy.contrib.rl.optimizers import *
 from alchemy.contrib.rl.streams import *
