1ad56deb207fb385a5220551203a54720f86f77d
Sam Wenke <samwenke@gmail.com>
2018-05-08 16:16:41 -0400

    remove some things

--- alchemy/contrib/rl/__init__.py
+++ alchemy/contrib/rl/__init__.py
@@ -5,5 +5,4 @@ from alchemy.contrib.rl.dataset import *
 from alchemy.contrib.rl.experience import *
 from alchemy.contrib.rl.core_ops import *
 from alchemy.contrib.rl.streams import *
-from . import envs as envs
 from . import pg as pg
