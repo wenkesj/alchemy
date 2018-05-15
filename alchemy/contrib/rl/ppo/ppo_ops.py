6e587e9f902ded35b08a0d112f2bdf1d8c55f7a6
Sam Wenke <samwenke@gmail.com>
2018-05-15 16:45:59 -0400

    update doc for gae

--- alchemy/contrib/rl/ppo/ppo_ops.py
+++ alchemy/contrib/rl/ppo/ppo_ops.py
@@ -24,18 +24,17 @@ def generalized_advantage_estimate(rewards,
                                    weights=1.,
                                    discount=.9,
                                    lambda_td=.95,
-                                   normalize_advantages=False,
                                    time_major=False):
   """Computes the GAE algorithm.
 
   Arguments:
     rewards: 1D or 2D Tensor, contiguous sequence(s) of rewards.
+    sequence_length: 0D or 1D Tensor, the length of the `rewards` sequence(s).
     values: 1D or 2D Tensor, contiguous sequence(s) of value estimates.
     max_sequence_length: `int` or `list`, maximum length(s) of rewards.
     weights: `tf.Tensor`, the weights/mask to apply to the result.
     discount: 0D scalar, the discount factor (gamma).
     lambda_td: 0D scalar, the td(lambda) factor (lambda).
-    normalize_advantages: `Boolean`, if the advantages should be normalized.
     time_major: `Boolean`, if rewards is 2D and already time_major, i.e. [time, batch_size].
 
   Returns:
