3aef62783985087a827dc97724472d856f8a6562
Sam Wenke <samwenke@gmail.com>
2018-05-21 17:29:26 -0400

    use a2c from original paper

--- alchemy/contrib/rl/pg/pg_ops_test.py
+++ alchemy/contrib/rl/pg/pg_ops_test.py
@@ -356,9 +356,141 @@ class PGTest(test.TestCase):
             deterministic=True, save_replay=False)
         print('average_rewards = {}'.format(rewards / PGTest.hparams.num_episodes))
 
+  @test_util.skip_if(True)
+  def test_pg_ops_a2c(self):
+    ops.reset_default_graph()
+    np.random.seed(42)
+    random_seed.set_random_seed(42)
+    env = gym.make('CartPole-v0')
+    env.seed(42)
+
+    # Setup the policy and model
+    global_step = training_util.get_or_create_global_step()
+    deterministic_ph = array_ops.placeholder(
+        dtypes.bool, [], name='deterministic')
+    exploration_op = learning_rate_decay.exponential_decay(
+        PGTest.hparams.initial_exploration,
+        global_step,
+        PGTest.hparams.exploration_decay_steps,
+        PGTest.hparams.exploration_decay_rate)
+
+    state_distribution, state_ph = gym_ops.distribution_from_gym_space(
+        env.observation_space, name='state_space')
+
+    # values
+    with variable_scope.variable_scope('logits') as vs:
+      state_body_op = mlp(state_ph, PGTest.hparams.hidden_layers)
+      if PGTest.hparams.use_dropout_exploration:
+        state_body_op = core.Dropout(exploration_op)(
+            state_body_op, gen_math_ops.logical_not(deterministic_ph))
+      action_distribution, action_value_op = gym_ops.distribution_from_gym_space(
+          env.action_space, logits=[state_body_op], name='action_space')
+      if PGTest.hparams.use_dropout_exploration:
+        action_op = array_ops.squeeze(action_distribution.mode())
+      else:
+        action_op = array_ops.squeeze(sampling_ops.epsilon_greedy(
+            action_distribution, exploration_op, deterministic_ph))
+      value_body_op = core.dense(
+          state_body_op, units=PGTest.hparams.value_units,
+          activation=nn_ops.relu, use_bias=False)
+      value_op = array_ops.squeeze(core.dense(value_body_op, units=1, use_bias=False), -1)
+      policy_variables = ops.get_collection(
+          ops.GraphKeys.GLOBAL_VARIABLES, scope=vs.name)
+
+
+    # Setup the dataset
+    stream = streams.Uniform.from_distributions(
+        state_distribution, action_distribution,
+        with_values=True)
+    replay_dataset = dataset.ReplayDataset(
+        stream, max_sequence_length=PGTest.hparams.max_sequence_length)
+    replay_dataset = replay_dataset.batch(PGTest.hparams.batch_size)
+    replay_dataset = replay_dataset.repeat(PGTest.hparams.replay_epochs)
+    replay_op = replay_dataset.make_one_shot_iterator().get_next()
+
+
+    action_ph = array_ops.placeholder(
+        stream.action_dtype,
+        [None, None] + stream.action_shape,
+        name='action')
+    value_ph = array_ops.placeholder(
+        stream.reward_dtype,
+        [None, None] + stream.reward_shape,
+        name='value')
+    reward_ph = array_ops.placeholder(
+        stream.reward_dtype,
+        [None, None] + stream.reward_shape,
+        name='reward')
+    terminal_ph = array_ops.placeholder(
+        dtypes.bool,
+        [None, None],
+        name='terminal')
+    sequence_length_ph = array_ops.placeholder(
+        dtypes.int32,
+        [None, 1],
+        name='sequence_length')
+    sequence_length = array_ops.squeeze(sequence_length_ph, -1)
+
+
+    # Setup the loss/optimization procedure
+    advantage_op, return_op = pg_ops.generalized_advantage_estimate(
+        reward_ph,
+        value_ph,
+        sequence_length,
+        max_sequence_length=PGTest.hparams.max_sequence_length,
+        weights=(1 - math_ops.cast(terminal_ph, reward_ph.dtype)),
+        discount=PGTest.hparams.discount,
+        lambda_td=PGTest.hparams.lambda_td)
+
+    actor_loss_op = -action_distribution.log_prob(action_ph) * advantage_op
+    critic_loss_op = math_ops.square(return_op - value_op) * PGTest.hparams.value_coeff
+    entropy_loss_op = -action_distribution.entropy(name='entropy') * PGTest.hparams.entropy_coeff
+    loss_op = entropy_loss_op + actor_loss_op - critic_loss_op
+    loss_op = -math_ops.reduce_mean(
+        math_ops.reduce_sum(loss_op, -1) / math_ops.cast(
+                sequence_length, actor_loss_op.dtype))
+
+    optimizer = adam.AdamOptimizer(
+        learning_rate=PGTest.hparams.learning_rate)
+    train_op = optimizer.minimize(loss_op, var_list=policy_variables)
+
+    with self.test_session() as sess:
+      sess.run(variables.global_variables_initializer())
+
+      for iteration in range(PGTest.hparams.num_iterations):
+        rewards = gym_test_utils.rollout_with_values_on_gym_env(
+            sess, env, state_ph, deterministic_ph,
+            action_value_op, action_op, value_op,
+            num_episodes=PGTest.hparams.num_episodes,
+            stream=stream)
+
+        while True:
+          try:
+            replay = sess.run(replay_op)
+          except (errors_impl.InvalidArgumentError, errors_impl.OutOfRangeError) as e:
+            break
+          _, loss = sess.run(
+              (train_op, loss_op),
+              feed_dict={
+                state_ph: replay.state,
+                action_ph: replay.action,
+                value_ph: replay.value,
+                reward_ph: replay.reward,
+                terminal_ph: replay.terminal,
+                sequence_length_ph: replay.sequence_length,
+                deterministic_ph: True,
+              })
+
+        rewards = gym_test_utils.rollout_on_gym_env(
+            sess, env, state_ph, deterministic_ph,
+            action_value_op, action_op,
+            num_episodes=PGTest.hparams.num_episodes,
+            deterministic=True, save_replay=False)
+        print('average_rewards = {}'.format(rewards / PGTest.hparams.num_episodes))
+
   # @test_util.skip_if(True)
   def test_pg_ops_meta_rl(self):
-    """This tests the PPO algorithm with GAE for meta-RL on the dependent bandits tasks.
+    """This tests the A2C algorithm with GAE for meta-RL on the dependent bandits tasks.
 
     Reference:
       https://arxiv.org/abs/1611.05763
@@ -469,36 +601,6 @@ class PGTest(test.TestCase):
       policy_variables = ops.get_collection(
           ops.GraphKeys.GLOBAL_VARIABLES, scope=vs.name)
 
-    # target
-    with variable_scope.variable_scope('old_logits') as vs:
-      old_state_body_op, old_internal_state_op, old_internal_state_ph, old_zero_state_fn = rnn(
-          state_in, PGTest.hparams.hidden_layers)
-
-      if PGTest.hparams.use_dropout_exploration:
-        old_state_body_op = core.Dropout(exploration_op)(
-            old_state_body_op, gen_math_ops.logical_not(deterministic_ph))
-
-      old_action_distribution, old_action_value_op = gym_ops.distribution_from_gym_space(
-          train_envs['medium'].action_space, logits=[old_state_body_op], name='action_space')
-
-      if PGTest.hparams.use_dropout_exploration:
-        old_action_op = array_ops.squeeze(old_action_distribution.mode())
-      else:
-        old_action_op = array_ops.squeeze(sampling_ops.epsilon_greedy(
-            old_action_distribution, exploration_op, deterministic_ph))
-      old_value_body_op = core.dense(
-          old_state_body_op, units=PGTest.hparams.value_units,
-          activation=nn_ops.relu, use_bias=False)
-      old_value_op = array_ops.squeeze(
-          core.dense(old_value_body_op, units=1, use_bias=False), -1)
-      old_policy_variables = ops.get_collection(
-          ops.GraphKeys.GLOBAL_VARIABLES, scope=vs.name)
-
-
-    assign_policy_op = control_flow_ops.group(*list(state_ops.assign(t, f)
-                                                    for f, t in zip(
-                                                        policy_variables, old_policy_variables)))
-
 
     # Setup the loss/optimization procedure
     advantage_op, return_op = pg_ops.generalized_advantage_estimate(
@@ -510,20 +612,13 @@ class PGTest(test.TestCase):
         discount=PGTest.hparams.discount,
         lambda_td=PGTest.hparams.lambda_td)
 
-    # actor loss
-    logits_prob = -action_distribution.log_prob(action_ph)
-    old_logits_prob = -old_action_distribution.log_prob(action_ph)
-    ratio = math_ops.exp(logits_prob - old_logits_prob)
-    clipped_ratio = clip_ops.clip_by_value(
-        ratio, 1. - PGTest.hparams.epsilon, 1. + PGTest.hparams.epsilon)
-
-    actor_loss_op = -math_ops.minimum(ratio * advantage_op, clipped_ratio * advantage_op)
+    actor_loss_op = -action_distribution.log_prob(action_ph) * advantage_op
     critic_loss_op = math_ops.square(return_op - value_op) * PGTest.hparams.value_coeff
     entropy_loss_op = -action_distribution.entropy(name='entropy') * PGTest.hparams.entropy_coeff
-    loss_op = entropy_loss_op - actor_loss_op - critic_loss_op
+    loss_op = entropy_loss_op + actor_loss_op - critic_loss_op
     loss_op = -math_ops.reduce_mean(
         math_ops.reduce_sum(loss_op, -1) / math_ops.cast(
-                sequence_length, logits_prob.dtype))
+                sequence_length, actor_loss_op.dtype))
 
     optimizer = adam.AdamOptimizer(
         learning_rate=PGTest.hparams.learning_rate)
@@ -531,20 +626,18 @@ class PGTest(test.TestCase):
 
     with session.Session() as sess:
       sess.run(variables.global_variables_initializer())
-      sess.run(assign_policy_op)
 
       for iteration in range(PGTest.hparams.num_iterations):
         for train_name, train_env in train_envs.items():
           rewards = gym_test_utils.rollout_meta_with_values_on_gym_env(
-              sess, train_env, state_ph, old_internal_state_ph,
+              sess, train_env, state_ph, internal_state_ph,
               action_ph, reward_ph, deterministic_ph,
-              old_action_value_op, old_action_op,
-              old_value_op, old_internal_state_op,
-              old_zero_state_fn, initial_action,
+              action_value_op, action_op,
+              value_op, internal_state_op,
+              zero_state_fn, initial_action,
               num_episodes=PGTest.hparams.num_meta_train_episodes,
               stream=stream)
 
-        sess.run(assign_policy_op)
         while True:
           try:
             replay = sess.run(replay_op)
@@ -562,7 +655,6 @@ class PGTest(test.TestCase):
                 terminal_ph: replay.terminal,
                 sequence_length_ph: replay.sequence_length,
                 **{k: v for k, v in zip(internal_state_ph, internal_state)},
-                **{k: v for k, v in zip(old_internal_state_ph, internal_state)},
                 deterministic_ph: True,
               })
 
